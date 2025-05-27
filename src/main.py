cdd8be76bb868733573d491948c0ff05c4571834
PabloEmilioAlonsoRomero <157993849+PabloAlonsoRomero@users.noreply.github.com>
2025-05-27 17:39:35 -0600

    Creacion de imagenes para pruebas de sustitucion

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import funciones as fn
+
+# Prueba imagen original
+imagenOriginal = np.array([[1,2,3,4,5],
+                           [6,7,8,9,10],
+                           [11,12,13,14,15],
+                           [16,17,18,19,20],
+                           [21,22,23,24,25]])
+
+# Objeto a buscar
+objetoABuscar = np.array([[7,8],
+                          [12,13]])
+
+# Objeto a pegar
+objetoAPegar = np.array([[99,100],
+                          [101,102]])
